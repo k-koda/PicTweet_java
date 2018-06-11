5a5a0916804d6bc5be557078b38a5ddf8cb236aa
k-koda <keisuke.physi@gmail.com>
2018-06-11 23:19:10 +0900

    データベース上の画像とツイートを表示

--- src/main/java/com/example/web/controller/TweetController.java
+++ src/main/java/com/example/web/controller/TweetController.java
@@ -1,5 +1,29 @@
 package com.example.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.business.domain.Tweet;
+import com.example.business.repository.TweetRepository;
+import java.util.List;
+
+@Controller
 public class TweetController {
+
+    @Autowired
+    private TweetRepository tweetRepository;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView index(ModelAndView mav) {
+      //String hello = "Hello, Spring Boot!";
+      List<Tweet> tweets = tweetRepository.findAll();
+      mav.addObject("tweets", tweets);
+      mav.setViewName("tweet/index");
+      return mav;
+    }
+
 
 }
